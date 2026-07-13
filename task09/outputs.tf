c524f383c20ca01e6a0d89c97958215a991a49b8
Sardor <tashkhodjaev.sardor@gmail.com>
2026-07-13 16:30:01 +0500

    outputs

--- task09/outputs.tf
+++ task09/outputs.tf
@@ -1,4 +1,4 @@
-/* output "azure_firewall_public_ip" {
+output "azure_firewall_public_ip" {
     description = "Azure Firewall Public IP address"
     value = 
   
@@ -7,4 +7,4 @@
 output "azure_firewall_private_ip" {
   description = "Azure Firewall Private IP address"
 value = 
-} */
+}
